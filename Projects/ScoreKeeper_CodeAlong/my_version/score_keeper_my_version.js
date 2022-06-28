d27c6e687a63702e5581e6fe9607580ddf4c9a9a
devk <devk@umich.edu>
2022-06-28 08:50:40 -0400

    reformatting

--- Projects/ScoreKeeper_CodeAlong/my_version/score_keeper_my_version.js
+++ Projects/ScoreKeeper_CodeAlong/my_version/score_keeper_my_version.js
@@ -8,46 +8,46 @@ var blueInt = 0;
 var redInt = 0;
 var limit = 0;
 var game = false;
-function score_limit(blue,red){
-    if(blueInt == limit){
-        alert("blue wins, press reset to play again");
-        game = true;
-        return;
-    } else if(redInt==limit){
-        alert("red wins, press reset to play again");
-        game = true;
-        return;
-    } else {
-        return;
-    }
+function score_limit(blue, red) {
+  if (blueInt == limit) {
+    alert("blue wins, press reset to play again");
+    game = true;
+    return;
+  } else if (redInt == limit) {
+    alert("red wins, press reset to play again");
+    game = true;
+    return;
+  } else {
+    return;
+  }
 }
-valueLimit.addEventListener('input',function(){
-    limit = parseInt(this.value);
-})
-blueIncr.addEventListener('click', function(){
-    if (game==false){
-        blueInt++;
-        blueScore.innerText = blueInt;
-        score_limit(blueInt,redInt);
-    } else {
-        alert("reset the game")
-        return;
-    }
+valueLimit.addEventListener("input", function () {
+  limit = parseInt(this.value);
 });
-redIncr.addEventListener('click', function(){
-    if (game==false){
+blueIncr.addEventListener("click", function () {
+  if (game == false) {
+    blueInt++;
+    blueScore.innerText = blueInt;
+    score_limit(blueInt, redInt);
+  } else {
+    alert("reset the game");
+    return;
+  }
+});
+redIncr.addEventListener("click", function () {
+  if (game == false) {
     redInt++;
     redScore.innerText = redInt;
-    score_limit(blueInt,redInt);
-    } else {
-        alert("reset the game")
-        return;
-    }
+    score_limit(blueInt, redInt);
+  } else {
+    alert("reset the game");
+    return;
+  }
 });
-reset.addEventListener('click', function(){
-    redInt = 0;
-    blueInt = 0;
-    redScore.innerText = redInt;
-    blueScore.innerText = blueInt;
-    game = false;
+reset.addEventListener("click", function () {
+  redInt = 0;
+  blueInt = 0;
+  redScore.innerText = redInt;
+  blueScore.innerText = blueInt;
+  game = false;
 });
